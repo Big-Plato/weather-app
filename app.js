c19d9cd3f19f900e8247baf68b75f14aae6a0ae5
Big-Plato <luisoctavius.sc@gmail.com>
2025-02-17 18:48:34 -0300

    trying to change the function getData to deal with the next days (not working yet)

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 import { fetchData } from "./modules/fetchData.js"
-import { getData } from "./modules/Controller.js"
+import { getData, createWeatherInfo, contentSection } from "./modules/controller.js"
 
 const search = document.querySelector("#search-text");
 const searchBtn = document.querySelector("#search-button");
 
 searchBtn.addEventListener("click", () => {
+    contentSection.innerHTML = "";
     fetchData(search.value).then(function (result) {
+    console.log(result)
     const weatherInfo = getData(result);
-    console.log(weatherInfo);
-    })
+    const nextDays = result.days;
+    console.log(nextDays[1])
+    createWeatherInfo(weatherInfo);
+    for (let i = 1; i <= 5; i++) {
+        console.log(result.days[i])
+        const info = getData(result.days[i]);
+        console.log(info)
+        createWeatherInfo(info);
+    }
+    
+    });
 })
+
+
