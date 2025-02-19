2a04ce075ac2450213d5020a649dc0bdbe71824c
Big-Plato <luisoctavius.sc@gmail.com>
2025-02-19 19:17:28 -0300

    fix: feelslike reads undefined in first card info

--- modules/controller.js
+++ modules/controller.js
@@ -2,10 +2,8 @@ export const contentSection = document.querySelector("#content");
 
 export function getData(data) {
   let address = data.datetime || data.resolvedAddress;
-  console.log(address);
   let description = data.description;
   let conditions = data.currentConditions.conditions || data.conditions;
-  console.log(conditions)
   let sensationTerm =  data.feelslike || data.currentConditions.feelslike;
   let temp = data.temp || data.currentConditions.temp;
   let icon = data.icon || data.currentConditions.icon;
@@ -41,11 +39,12 @@ export function createWeatherInfo(obj) {
   const paraAddress = createElement("p");
   divCard.appendChild(paraAddress);
   const address = obj.address || formatDate(obj.datetime);
+  const feelslike = obj.feelslike || obj.sensationTerm;
   paraAddress.innerHTML = `<p><strong>${address}</strong></p>
   <p><span><strong>Conditions:</strong> ${obj.conditions}</span></p>
-  <strong>Description:</strong><p> ${obj.description}</
+  <p><strong>Description:</strong> ${obj.description}</
   p>
-  <p><strong>Feels like:</strong> ${obj.sensationTerm}</p>`;
+  <p><strong>Feels like:</strong> ${feelslike}</p>`;
 }
 
 function createElement(element) {
