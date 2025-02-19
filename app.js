949e6718d6d50687d724e0af9c7e877800e4f710
Big-Plato <luisoctavius.sc@gmail.com>
2025-02-19 18:33:40 -0300

    add: loader circle

--- app.js
+++ app.js
@@ -11,26 +11,31 @@ import {
 
 const search = document.querySelector("#search-text");
 const searchBtn = document.querySelector("#search-button");
+const loaderCircle = document.querySelector(".loader-circle");
 
 // Everytime the user does a search, the city is saved
 // in localStorage and when the page opens again the place
 // that was saved is retrieved
 document.addEventListener("DOMContentLoaded", () => {
+  loadingCircle(1);
   const days = retrieveLocalStorage();
   const city = days[0].address.split(" ");
   // console.log(days)
   fetchData(city[0].replace(",", "")).then(function (result) {
    populate(getData(result), result)
-  });
+   loadingCircle(2);
+});
 });
 
 searchBtn.addEventListener("click", () => {
   contentSection.innerHTML = "";
+  loadingCircle(1);
   fetchData(search.value).then(function (result) {
     populate(getData(result), result)
     saveToLocalStorage(weatherInfo, nextDays);
-    
+    loadingCircle(2);
   });
+  
 });
 
 
@@ -41,8 +46,15 @@ function populate(fn, result) {
     createWeatherInfo(weatherInfo);
     for (let i = 1; i <= 5; i++) {
         const info = result.days[i];
-        console.log("Info " + info)
         createWeatherInfo(info)
       }
       return weatherInfo, nextDays;
+}
+
+function loadingCircle(num) {
+    if (num === 1) {
+        loaderCircle.style.cssText = "display: block;";
+    } else {
+        loaderCircle.style.cssText = "display: none;";
+    }
 }
